3ee1264fa56a22d54ff1602587b245e17d35c7fa
Aleksandra <aleksandra@omnimap.nl>
2024-10-10 20:36:38 +0200

    Remove redundant menuFetch call

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
 import MenuComponent from "@/components/menu/MenuComponent.vue";
 import Skeleton from "@/components/general/Skeleton.vue";
 
-import { useMenuStore } from "@/stores/menu";
-const menuStore = useMenuStore();
-
-onMounted(() => {
-  menuStore.fetchMenu();
-});
 </script>
 
 <template>
   <div>
     <Skeleton />
-    <MenuComponent :menu="menuStore.menu" class="menu" />
+    <MenuComponent class="menu" />
   </div>
 </template>
